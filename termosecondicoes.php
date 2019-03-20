d2dc2152dedc690e5d37ed02cb11bfe0cff5569c
danieladg98 <33914495+danieladg98@users.noreply.github.com>
2019-03-20 13:18:01 +0000

    termos e condicoes

---
+++ termosecondicoes.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>LOBO'S</title>
+
+    <?php
+      include_once 'parts/head.php';
+    ?>
+
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <?php
+    include_once 'parts/nav.php';
+  ?>
+
+            <!-- ****** Checkout Area Start ****** -->
+            <div class="checkout_area section_padding_20">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-12">
+                            <div class="checkout_details_area mt-50 clearfix">
+
+                                <div class="cart-page-heading">
+                                    <h4>TERMOS E CONDIÇÕES GERAIS DE VENDA DA LOJA ONLINE WWW.LOBOS.COM</h4>
+                                </div>
+                                
+                                <div class="">
+                                    <h6>1. TITULARIDADE DA LOJA ONLINE ANA SOUSA (WWW.ANASOUSA.COM)</h6>
+                                    <p>A MJJS – Comércio de Vestuário, S.A. (de agora em diante, MJJS ou simplesmente Vendedora), com sede na Rua 1º de Maio, nº 239, 4755-405 Pereira – Barcelos, Portugal, com o capital social de 2.250.000€, NIPC nº PT 504 939 866, registada na Conservatória do Registo Comercial de Barcelos sob o nº 3657, é a legitima proprietária do site oficial da marca ANA SOUSA e cujo domínio pode ser encontrado em http://www.anasousa.com.</p>
+                                </div>
+                                
+                                <div class="">
+                                    <h6>1. TITULARIDADE DA LOJA ONLINE ANA SOUSA (WWW.ANASOUSA.COM)</h6>
+                                    <p>A MJJS – Comércio de Vestuário, S.A. (de agora em diante, MJJS ou simplesmente Vendedora), com sede na Rua 1º de Maio, nº 239, 4755-405 Pereira – Barcelos, Portugal, com o capital social de 2.250.000€, NIPC nº PT 504 939 866, registada na Conservatória do Registo Comercial de Barcelos sob o nº 3657, é a legitima proprietária do site oficial da marca ANA SOUSA e cujo domínio pode ser encontrado em http://www.anasousa.com.</p>
+                                </div>
+
+                                <form action="#" method="post">
+                                    <div class="row">
+                   
+                                        <div class="col-12">
+                                            <div class="custom-control custom-checkbox d-block mb-2">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                                <label class="custom-control-label" for="customCheck1">Terms and conitions</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox d-block mb-2">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                                <label class="custom-control-label" for="customCheck2">Criar Conta</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox d-block">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                                <label class="custom-control-label" for="customCheck3">Subscribe to our newsletter</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- ****** Checkout Area End ****** -->
+
+            <?php
+    include_once 'parts/footer.php';
+  ?>
+
+    </div>
+    <!-- /.wrapper end -->
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+
+</body>
+
+</html>
